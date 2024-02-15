7c292a6f66120e524280e323be67682ac8aff2d7
Taras <topanasenko85@gmail.com>
2024-02-15 15:50:18 +0200

    all

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,80 @@
-const key = new Key();
+// const key = new Key();
 
+// const house = new MyHouse(key);
+// const person = new Person(key);
+
+// house.openDoor(person.getKey());
+
+// house.comeIn(person);
+
+
+// export {};
+
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} came in.`);
+    } else {
+      console.log('The door is closed.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log('The door is opened.');
+    } else {
+      console.log('Invalid key.');
+    }
+  }
+}
+
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
